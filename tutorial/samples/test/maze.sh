e4695d3592f31f04b52887ee61d771a2c05e2f3f
yangke <yangke2015@iscas.ac.cn>
2019-10-03 16:11:48 +0800

    add two scripts for running original AFL to do the hard problem solving work!

---
+++ tutorial/samples/test/maze.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+~/Program/AFL/afl-2.35b/afl-gcc ./maze.c -o maze_profiled
+cd ~/Program/AFL/aflgo/bak/aflgo-good/tutorial/samples/test
+rm -rf maze_findings
+mkdir ./maze_testcases
+#known answers
+#ssssddddwwaawwddddssssddwwww
+#ssssddddwwaawwddddsddwwdwww
+echo sssswwaawwddddssssddwwww > ./maze_testcases/answer1
+~/Program/AFL/afl-2.35b/afl-fuzz -i maze_testcases -m 100 -o maze_findings ./maze_profiled
+
---
+++ tutorial/samples/test/regex.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+~/Program/AFL/afl-2.35b/afl-gcc ./regex.c -o regex_profiled
+cd ~/Program/AFL/aflgo/bak/aflgo-good/tutorial/samples/test
+mkdir ./regex_testcases
+echo "abc" > ./regex_testcases/words
+~/Program/AFL/afl-2.35b/afl-fuzz -i regex_testcases -o regex_findings ./regex_profiled @@
